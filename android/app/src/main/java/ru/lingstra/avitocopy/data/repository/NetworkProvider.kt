d2d7e2d2e5efa71fd317ca8f857cf601c6e729aa
AlexSkvor <megoskvor95@yandex.ru>
2019-09-27 10:58:41 +0200

    redundant files deleted

--- android/app/src/main/java/ru/lingstra/avitocopy/data/repository/NetworkProvider.kt
+++
@@ -1,9 +0,0 @@
-package ru.lingstra.avitocopy.data.repository
-
-import io.reactivex.Observable
-
-interface NetworkProvider {
-    fun loadMore(set: MutableSet<SimplestUser>, level: Int): Boolean
-    fun getFriendItemsFromUrls(urls: Pair<String, String>): Pair<SimplestUser, SimplestUser>
-    fun queries(): Observable<Unit>
-}
